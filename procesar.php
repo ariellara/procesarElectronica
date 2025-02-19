4e37a6d9c6f7935556d767daa3750d446eb35d51
ariel.lara <ariel.lara@dolphining.com>
2025-02-18 21:22:44 -0500

    lis

--- procesar.php
+++ procesar.php
@@ -1,45 +1,45 @@
 <?php
-include ('negocio/funcionesElectronica.php');
+include('negocio/funcionesElectronica.php');
 include('negocio/enviarElectronica.php');
-include ('conexion/conexion.php');
+include('conexion/conexion.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (isset($_POST['data'])) {
-       
+
         $datosFactura = json_decode($_POST['data'], true);
         $insertarDatosFactura = guardarFactura($datosFactura["datosTiquet"], $conn, $datosFactura["cliente"]);
-        if(!$insertarDatosFactura["estado"])
-        {
+        if (!$insertarDatosFactura["estado"]) {
             $response = [
                 "estado" => 'error',
                 "mensaje" => 'El proceso de insertar facturas no se proceso.',
                 "cufe" => ''
             ];
+            mysqli_close($conn);
+            $cmd = null;
             echo json_encode($response);
         }
         $insertarDetallesFactura = guardarDetallesFactura($datosFactura["detallesTiquet"], $conn);
-        if(!$insertarDetallesFactura["estado"])
-        {
+        if (!$insertarDetallesFactura["estado"]) {
             $response = [
                 "estado" => 'error',
                 "mensaje" => 'El proceso de insertar detalles facturas no se proceso.',
                 "cufe" => ''
             ];
+            mysqli_close($conn);
+            $cmd = null;
             echo json_encode($response);
         }
-      $enviarFacturaElectronica = enviarFacturaElectronica($conn, $insertarDetallesFactura["numeroT"], $cmd);
+        $enviarFacturaElectronica = enviarFacturaElectronica($conn, $insertarDetallesFactura["numeroT"], $cmd);
 
         $response = [
             "estado" => 'success',
             "mensaje" => $enviarFacturaElectronica['mensaje'],
             "cufe" => $enviarFacturaElectronica['cufe']
         ];
-       // mysqli_close($conn);
-       // $cmd = null;
+        mysqli_close($conn);
+        $cmd = null;
         echo json_encode($response);
-    }
-    else{
-        sleep(3);
+    } else {
         echo json_encode([
             "estado" => 'error',
             "mensaje" => 'no se recibio los datos.',
